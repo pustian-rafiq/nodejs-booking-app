c6a928b91048b93c37961565bff9f2305d55c58d
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-06-11 18:38:15 +0600

    header search design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,42 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./header.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
-
+import { faBed, faCalendarDay, faCar, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { format } from 'date-fns';
 const Header = () => {
-  return (
-    <div className='header'>
-        <div className="headerContainer">
-        <div className="headerList">
-            <div className="headerListItem active">
-              <FontAwesomeIcon icon={faBed} />
-              <span>Stays</span>
+const [openDate, setOpenDate] = useState("")
+    const [date, setDate] = useState([
+        {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: 'selection'
+        }
+      ]);
+
+    return (
+        <div className='header'>
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem active">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Stays</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane} />
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faCar} />
+                        <span>Car Rentals</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Attractions</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi} />
+                        <span>Airport Taxis</span>
+                    </div>
+                </div>
+                <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+                <p className="headerDescription">
+                    Get rewarded for your travels - unlock instant savings of 10% or more with a free hotel booking account
+                </p>
+                <button className='headerBtn'>Sign in / Register</button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faBed} className="headerIcon" />
+                        <input 
+                          type="text"
+                          placeholder='Where are you going?'
+                          className='headerSearchInput'
+                          />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendarDay} className="headerIcon" />
+                        <span onClick={()=> setOpenDate(!openDate)} className='headerSearchText'>{`${format(date[0].startDate,"MM/dd/yyyy")} to ${format(date[0].endDate,"MM/dd/yyyy")}`}</span>
+                        {
+                            openDate && <DateRange
+                            editableDateInputs={true}
+                            onChange={item => setDate([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={date}
+                            className="date"
+                          />
+                        }
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+                        <span className='headerSearchText'>2 adults 2 children 1 room</span>
+                    </div>
+                    <div className="headerSearchItem">
+                        <button className='headerBtn'>Search</button>
+                    </div>
+                </div>
             </div>
-            <div className="headerListItem">
-              <FontAwesomeIcon icon={faPlane} />
-              <span>Flights</span>
-            </div>
-            <div className="headerListItem">
-              <FontAwesomeIcon icon={faCar} />
-              <span>Car Rentals</span>
-            </div>
-            <div className="headerListItem">
-              <FontAwesomeIcon icon={faBed} />
-              <span>Attractions</span>
-            </div>
-            <div className="headerListItem">
-              <FontAwesomeIcon icon={faTaxi} />
-              <span>Airport Taxis</span>
-            </div>
-        </div>  
-        <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
-        <p className="headerDescription">
-            Get rewarded for your travels - unlock instant savings of 10% or more with a free hotel booking account
-        </p>
-        <button className='headerBtn'>Sign in / Register</button>            
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
