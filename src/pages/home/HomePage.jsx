17be6c4d303ab91b6fdb394a9c763cf443f8ecef
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-06-11 16:22:25 +0600

    design navbar

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
 
 const HomePage = () => {
   return (
     <div>
-        gjhhghjhgk
+       <Navbar/>
     </div>
   )
 }
